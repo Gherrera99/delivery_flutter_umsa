1226125cff111970c061c744b214a3494b721c0b
JOAQUINGALAVIZ <joaquingalaviz04@hotmail.com>
2024-05-08 01:53:55 -0600

    delivery.png

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -6,9 +6,23 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+      body: Stack(
+        children: [
+          _imageCover()
+        ],
       ),
     );
+  }
+
+
+//PRIVADO
+  Widget _imageCover() {
+    return  Container(
+      child: Image.asset(
+        'assets/img/delivery.png',
+        width: 130,
+        height: 130,
+      ), //image_asset
+    ); //Container
   }
 }
