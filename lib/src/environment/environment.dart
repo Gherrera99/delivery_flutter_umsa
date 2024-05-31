9599d5364bfce4c93db68cadff2c249284ddcd30
JOAQUINGALAVIZ <joaquingalaviz04@hotmail.com>
2024-05-31 14:22:34 -0600

    52. Creando la tabla categorias y insertando datos

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -1,5 +1,5 @@
 class Environment{
-  static const String API_URL = "http://192.168.1.90:3000/";
-  static const String API_URL_OLD = "http://192.168.1.90:3000/";
+  static const String API_URL = "http://192.168.1.900:3000/";
+  static const String API_URL_OLD = "http://192.168.1.900:3000/";
 
 }
