24ad653dad16e5b64d8fec2a2b2f12f2331cec66
JOAQUINGALAVIZ <joaquingalaviz04@hotmail.com>
2024-05-31 20:55:22 -0600

    53. Creando el formulario para crear una nueva categoria

--- lib/src/pages/client/products/list/client_products_list_page.dart
+++ lib/src/pages/client/products/list/client_products_list_page.dart
@@ -13,49 +13,12 @@ class ClientProductsListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      bottomNavigationBar: _bottomBar(),
-      body: Obx(() => IndexedStack(
-        index: con.indexTab.value,
-        children: [
-          RestaurantOrdersListPage(),
-          DeliveryOrdersListPage(),
-          ClientProfileInfoPage()
-        ],
-      ))
+      body: Center(child: Text('CLIENT PROFUCTS LIST')),
     );
   }
 
-  Widget _bottomBar(){
-    return Obx(() => CustomAnimatedBottomBar(
-      containerHeight: 70,
-      backgroundColor: Colors.amber,
-      showElevation: true,
-      itemCornerRadius: 24,
-      curve: Curves.easeIn,
-      selectedIndex: con.indexTab.value,
-      onItemSelected: (index) => con.changeTab(index),
-      items: [
-        BottomNavyBarItem(
-            icon: Icon(Icons.apps),
-            title: Text('Home'),
-            activeColor: Colors.white,
-            inactiveColor: Colors.black
-        ),
-        BottomNavyBarItem(
-            icon: Icon(Icons.list),
-            title: Text('Mis pedidos'),
-            activeColor: Colors.white,
-            inactiveColor: Colors.black
-        ),
-        BottomNavyBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Perfil'),
-            activeColor: Colors.white,
-            inactiveColor: Colors.black
-        ),
-      ],
-    ));
-  }
+
 
 }
