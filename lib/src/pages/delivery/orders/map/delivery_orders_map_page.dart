79f6278377d0d9ab768292d185632f2fc7dff734
JOAQUINGALAVIZ <joaquingalaviz04@hotmail.com>
2024-06-04 08:34:50 -0600

    97. Mostrando la informacion en el mapa de la orden

--- lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
+++ lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
@@ -12,72 +12,195 @@ class DeliveryOrdersMapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx (() => Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(
-            color: Colors.black
-        ),
-        title: Text(
-          'Ubica tu punto de referencia en el mapa',
+    return GetBuilder<DeliveryOrdersMapController> (
+        builder: (value) => Scaffold(
+          backgroundColor: Colors.grey[900],
+          body: Stack(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: _googleMaps()
+              ),
+              SafeArea(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonBack(),
+                        _iconCenterMyLocation(),
+                      ],
+                    ),
+                    Spacer(),
+                    _cardOrderInfo(context),
+                  ],
+                ),
+              ),
+              // _buttonAccept(context)
+            ],
+          ),
+        ));
+  }
+
+  Widget _buttonAccept(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 30, right: 30),
+      child: ElevatedButton(
+        onPressed: () => con.selectRefPoint(context),
+        child: Text(
+          'ENTREGAR PEDIDO',
           style: TextStyle(
               color: Colors.black
           ),
         ),
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)
+            ),
+            padding: EdgeInsets.all(15)
+        ),
+
       ),
-      body: Stack(
-        children: [
-          _googleMaps(),
-          _iconMyLocation(),
-          _cardAddress(),
-          _buttonAccept(context),
-        ],
-      ),
-    ));
+    );
   }
 
-  Widget _buttonAccept(BuildContext context){
+
+
+  Widget _cardOrderInfo(BuildContext context) {
     return Container(
-      alignment: Alignment.bottomCenter,
-      margin: EdgeInsets.only(bottom: 30),
+      height: MediaQuery.of(context).size.height * 0.4,
       width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () => con.selectRefPoint(context),
-        child: Text('SELECCIONAR ESTE PUNTO',
+      decoration: BoxDecoration(
+          color: Colors.grey[900],
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            topLeft: Radius.circular(20),
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 4,
+                blurRadius: 6,
+                offset: Offset(0, 3)
+            )
+          ]
+      ),
+      child: Column(
+        children: [
+          _listTileAddress(
+              con.order.address?.neighborhood ?? '',
+              'Barrio',
+              Icons.my_location
+          ),
+          _listTileAddress(
+              con.order.address?.address ?? '',
+              'Direccion',
+              Icons.location_on
+          ),
+          Divider(color: Colors.grey, endIndent: 30, indent: 30),
+          _clientInfo(),
+          _buttonAccept(context)
+        ],
+      ),
+    );
+  }
+
+
+  Widget _listTileAddress(String title, String subtitle, IconData iconData) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: ListTile(
+        title: Text(
+          title,
           style: TextStyle(
-            color: Colors.black
+              fontSize: 13,
+              color: Colors.white
           ),
         ),
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30)
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(
+              color: Colors.white
           ),
-          padding: EdgeInsets.all(15),
+        ),
+        trailing: Icon(iconData, color: Colors.white,),
+      ),
+    );
+  }
+
+  Widget _iconCenterMyLocation() {
+    return GestureDetector(
+      //con.centerPosition()
+      onTap: () => {},
+      child: Container(
+        alignment: Alignment.centerRight,
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        child: Card(
+          shape: CircleBorder(),
+          color: Colors.white,
+          elevation: 4,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              Icons.location_searching,
+              color: Colors.grey[600],
+              size: 20,
+            ),
+          ),
         ),
       ),
     );
   }
 
 
-  Widget _cardAddress(){
+  Widget _clientInfo() {
     return Container(
-      width: double.infinity,
-      alignment: Alignment.topCenter,
-      margin: EdgeInsets.symmetric(vertical: 30),
-      child: Card(
-        color: Colors.grey[800],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)
-        ),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Text(
-            con.addressName.value,
+      margin: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+      child: Row(
+        children: [
+          _imageClient(),
+          SizedBox(width: 15),
+          Text(
+            '${con.order.client?.name ?? ''} ${con.order.client?.lastname ?? ''}',
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.bold
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold
             ),
+            maxLines: 1,
           ),
+          Spacer(),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                color: Colors.grey[200]
+            ),
+            child: IconButton(
+              //=> con.callNumber()
+              onPressed: () {},
+              icon: Icon(Icons.phone, color: Colors.black),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageClient() {
+    return Container(
+      height: 50,
+      width: 50,
+      // padding: EdgeInsets.all(2),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: FadeInImage(
+          image: con.order.client!.image != null
+              ? NetworkImage(con.order.client!.image!)
+              : AssetImage('assets/img/no-image.png') as ImageProvider,
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 50),
+          placeholder:  AssetImage('assets/img/no-image.png'),
         ),
       ),
     );
@@ -95,6 +218,20 @@ class DeliveryOrdersMapPage extends StatelessWidget {
     );
   }
 
+  Widget _buttonBack() {
+    return Container(
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(left: 20),
+      child: IconButton(
+        onPressed: () => Get.back(),
+        icon: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+          size: 30,
+        ),
+      ),
+    );
+  }
 
   Widget _googleMaps() {
     return GoogleMap(
